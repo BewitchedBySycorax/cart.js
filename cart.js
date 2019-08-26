3e17fe73ce9884e8b2c6578cfcbf44a577c21be6
Alexey Voytovsky <al.voytovsky@gmail.com>
2019-08-26 21:05:29 +0300

    'getProductById' method added.

--- cart.js
+++ cart.js
@@ -48,19 +48,40 @@ var products = [{
 var cart = {
 	products: [],
 	sum: 0,
+	getProductById(productId) {
+		for (var i = 0; i < this.products.length; i++ ) {
+			if ( this.products[i].id == productId ) {
+				return this.products[i];
+			}
+		}
+
+		return false;
+	},
 	addProduct: function(product) {
-		var item;
-		item.quantity = 1;
-		item.id = product.id;
-		item.price = product.price;
-		this.products(push.item);
+		var item = this.getProductById(product.id);
+
+		if ( !item ) {
+			item = {};
+			item.quantity = 1;
+			item.id = product.id;
+			item.price = product.price;
+			this.products.push(item);
+		} else {
+			++item.quantity;
+		}
 		this.countTotalPrice();
 	},
 	countTotalPrice() {
 		var sum = 0;
 			for ( var i = 0; i < this.products.length; i++ ) {
-				sum += this.products[i].price;
+				sum += this.products[i].price * this.products[i].quantity;
 			}
 			this.sum = sum;
 	},
 };
+
+cart.addProduct(products[0]);
+cart.addProduct(products[0]);
+cart.addProduct(products[0]);
+cart.addProduct(products[1]);
+cart.addProduct(products[2]);
